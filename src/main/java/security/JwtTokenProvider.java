47f11fc3c6d1b95ffbf7d2839c9ad69bb399b850
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-03-24 22:56:02 +0300

    jwt token.

---
+++ src/main/java/security/JwtTokenProvider.java
@@ -0,0 +1,53 @@
+package security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${questapp.app.secret}")
+    private String APP_SECRET;
+
+    @Value("${questapp.expires.in}")
+    private long EXPIRES_IN;
+
+    public String generateJwtToken(Authentication auth){
+        JwtUserDetails userDetails = (JwtUserDetails) auth.getPrincipal();
+        Date expireDate = new Date(new Date().getTime() + EXPIRES_IN);
+        return Jwts.builder().setSubject(Long.toString(userDetails.getId()))
+                .setIssuedAt(new Date()).setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS512, APP_SECRET).compact();
+    }
+
+    public Claims parseJwt(String token) {
+        Jws<Claims> jws = Jwts.parser()
+                .setSigningKey(APP_SECRET)
+                .parseClaimsJws(token);
+        return jws.getBody();
+    }
+
+    public Long getUserIdFromJwt(String token){
+        Claims claims = parseJwt(token);
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String token){
+        try {
+            parseJwt(token);
+            return !isTokenExpired(token);
+        }catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException |
+                IllegalArgumentException e){
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = parseJwt(token).getExpiration();
+        return expiration.before(new Date());
+    }
+}
--- src/main/java/services/UserDetailsServiceImplementation.java
+++ src/main/java/services/UserDetailsServiceImplementation.java
@@ -4,9 +4,11 @@ import model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import repository.UserRepository;
 import security.JwtUserDetails;
 
+@Service
 public class UserDetailsServiceImplementation implements UserDetailsService {
 
     private UserRepository userRepository;
